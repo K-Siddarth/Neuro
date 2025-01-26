321e75bbfdecf4d937950f97c9c3f86121aae2dd
K-Siddarth <siddarth.kadambari@gmail.com>
2025-01-26 12:22:07 +0530

    Test commit

--- src/components/AchievementsPage.jsx
+++ src/components/AchievementsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import "./App.css";
+import "./AchievementsPage.css";
 
 const eventsData = [
   // {
--- src/components/Typing.jsx
+++ src/components/Typing.jsx
@@ -1,12 +1,17 @@
-import { ReactTyped, Typed } from "react-typed";
+import { ReactTyped } from "react-typed";
 
 const Typing = () => (
-  <div>
-    <ReactTyped strings={["Hello from Neuromancers", "The programming society of IIT Bhubaneswar"]} typeSpeed={70} backDelay={1070} backSpeed={70} loop style={{
-        // "fontFamily": "cursive",
-        "fontSize": "3rem"
-    }}/>
+  <div className="typing-container">
+    <ReactTyped
+      strings={["Hello from Neuromancers", "The programming society of IIT Bhubaneswar"]}
+      typeSpeed={70}
+      backDelay={1070}
+      backSpeed={70}
+      loop
+      className="typing-text"
+    />
   </div>
 );
 
-export default Typing
+export default Typing;
+
